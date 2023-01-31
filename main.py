d48fdd73c3f6bcc5b7b68e667cff27c726d6aeb4
Your Name <you@example.com>
2023-01-31 12:30:06 +0200

    Added second player.

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import random
 
-unique_number = random.randint(1,6)
-unique_numbers = random.randint(1,6)
+value_1 = random.randint(1,6)
+value_2 = random.randint(1,6)
+value_3 = random.randint(1,6)
+value_4 = random.randint(1,6)
 
-print (unique_numbers, unique_numbers)
+
+
+if value_1  + value_2 > value_3 + value_4:
+    print("Виграв перший гравець")
+
+elif value_1  + value_2 < value_3 + value_4:
+    print("Виграв другий гравець")
+
+else:
+    print("Нічія")
